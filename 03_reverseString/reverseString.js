ee793c27c99bbdda7e73ec336a5277df91aa4712
davidajimati <ajimatidavid@gmail.com>
2023-06-16 16:17:37 +0100

    reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  new_string = "";
+  // if (!word) return "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    new_string += word[i];
+  }
+  return new_string;
 };
 
 // Do not edit below this line
