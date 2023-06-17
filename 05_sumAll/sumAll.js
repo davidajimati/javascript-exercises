a7a4be5231edf250b014d671197edac6db3848fe
davidajimati <ajimatidavid@gmail.com>
2023-06-17 23:06:42 +0100

    sumAll function concluded

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function (first, last) {
+  if (typeof (first) != 'number' || typeof (last) != 'number') {
+    return ("ERROR");
+  }
+  if (first < 0 || last < 0) {
+    return ("ERROR");
+  }
 
+  let answer = 0;
+  if (first < last) {
+    while (first <= last) {
+      answer += first;
+      first++;
+    }
+    return answer;
+  }
+
+  if (first > last) {
+    while (first >= last) {
+      answer += first;
+      first--;
+    }
+    return answer;
+  }
+
+
+  return answer;
 };
 
 // Do not edit below this line
