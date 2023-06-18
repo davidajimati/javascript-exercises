17be54800cd6735d7e9d150e792b408bc1d65e02
davidajimati <ajimatidavid@gmail.com>
2023-06-18 20:07:27 +0100

    tempConversion function concluded

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (Fahrenheit) {
+  let result = (Fahrenheit - 32) * 5 / 9;
+  return Number(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (Celsius) {
+  let result = (Celsius * 9 / 5) + 32;
+  return Number(result.toFixed(1));
 };
 
 // Do not edit below this line
