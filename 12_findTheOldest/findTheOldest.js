19dfffc8cbc07365ce97b239c502c62f3deb3732
davidajimati <ajimatidavid@gmail.com>
2023-06-18 23:01:44 +0100

    findTheOldest function in process

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  target = ""
+  targetAge = 0
+  people.forEach(item => {
+    let age = item.yearOfDeath - item.yearOfBirth;
+    if (age > targetAge) {
+      target = item.name;
+      targetAge = age;
+    }
+  });
+  return target;
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
+
+console.log(findTheOldest(people));
