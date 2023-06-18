9b390e51df76eff8a881489ed7724ae22f5a221b
davidajimati <ajimatidavid@gmail.com>
2023-06-18 22:19:25 +0100

    palindrom function created

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  string = string.replace(/[^a-zA-Z0-9]/g, "");
+  string = string.toLowerCase()
 
+  let reverseStr = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reverseStr += string[i];
+  }
+  return reverseStr === string;
 };
 
 // Do not edit below this line
