c95a717a43e525080351b59d935b83ddf81d9cf6
davidajimati <ajimatidavid@gmail.com>
2023-06-18 22:41:18 +0100

    getTheTitles function concluded

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+  let res = [];
 
+  array.forEach(item => {
+    res.push(item.title)
+  });
+  return res;
 };
 
 // Do not edit below this line
