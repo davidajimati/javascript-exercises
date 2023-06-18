bc060ab8ae089b6a8ffc118d4a6ed4bedd56c380
davidajimati <ajimatidavid@gmail.com>
2023-06-18 21:13:11 +0100

    calculator functions concluded

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let ans = 0;
+  array.forEach(item => {
+    ans += item;
+  });
+  return ans;
 };
 
-const multiply = function() {
-
+const multiply = function (...args) {
+  let ans = 1;
+  args.forEach(item => {
+    ans *= item;
+  })
+  return ans;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let ans = 1;
+  while (b > 0) {
+    ans *= a;
+    b--;
+  }
+  return ans;
 };
 
-const factorial = function() {
-	
+// NORMAL SIMPLE
+// const factorial = function (num) {
+//   if (num == 0) return 1;
+//   let ans = 1;
+//   while (num > 0) {
+//     ans *= num;
+//     num--;
+//   }
+//   return ans;
+// };
+
+ // RECURSIVE METHOD
+const factorial = function (num) {
+  if (num == 0 || num == 1) return 1;
+  return (num * factorial(num - 1));
 };
 
 // Do not edit below this line
